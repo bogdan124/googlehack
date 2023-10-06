76d871b2a25611fcedc7432de340f49a389da674
Bogdan <bogdan_bibina@yahoo.com>
2023-10-07 01:20:46 +0300

    adding chatbot and job predictor

--- chat_bot/chat_bot/main.py
+++ chat_bot/chat_bot/main.py
@@ -1,7 +1,100 @@
+from keras.models import model_from_json
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+from pydantic import BaseModel
+import tensorflow
+import random
 from fastapi import FastAPI
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import json
+# Load the model architecture from the JSON file
+with open("chatbot_model.json", "r") as json_file:
+    loaded_model_json = json_file.read()
+loaded_model = model_from_json(loaded_model_json)
+# Load the model weights
+loaded_model.load_weights("chatbot_model_weights.h5")
+
+# Compile the loaded model
+loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+nltk.download('punkt')
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    return numpy.array(bag)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+class TextRequest(BaseModel):
+    text: str
+
+@app.post("/")
+async def read_root(request_data: TextRequest):
+    text = request_data.text     
+    stemmer = LancasterStemmer()
+    with open('intents.json') as file:
+        data = json.load(file)
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+    for intent in data['intents']:
+        for pattern in intent['patterns']:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
+    training = []
+    output = []
+    out_empty = [0 for _ in range(len(labels))]
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+    training = numpy.array(training)
+    output = numpy.array(output)
+    inp = request_data.text
+    dataToPredict = bag_of_words(inp, words)
+    dataToPredict = numpy.array(dataToPredict)
+    dataToPredict.shape = (1, len(training[0]))
+    results = loaded_model.predict(dataToPredict, batch_size=None)
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+    if inp.lower() == "quit":
+        return {"response": "bye!"}
+    return {"response": random.choice(responses) }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Start the FastAPI application
+    uvicorn.run(app, host="0.0.0.0", port=8070)
--- jobs_recommended/jobs_recommended/main.py
+++ jobs_recommended/jobs_recommended/main.py
@@ -1,7 +1,60 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+import joblib  # for saving the model
 from fastapi import FastAPI
+from fastapi import BackgroundTasks
+from pydantic import BaseModel
+
+vectorizer = None
+knn = None
+jobs_df = None
+
+def read_data(csv_file_path):
+    df = pd.read_csv(csv_file_path)
+    return df[['Job Title', 'Job Description']]
+# Load data and extract job descriptions
+jobs_df = read_data("jobs.csv")
+job_descriptions = jobs_df['Job Description']
+# Vectorize job descriptions using TF-IDF
+vectorizer = TfidfVectorizer(stop_words='english')
+job_descriptions_vectorized = vectorizer.fit_transform(job_descriptions)
+# Initialize KNN model
+knn = NearestNeighbors(n_neighbors=5, metric='cosine', algorithm='brute')
+knn.fit(job_descriptions_vectorized)
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+class TextRequest(BaseModel):
+    text: str
+
+
+
+# Define a function to run on startup
+async def on_startup():
+    return {"knn":knn }
+
+@app.on_event("startup")
+async def startup_event():
+    await on_startup()
+
+@app.post("/")
+async def read_root(request_data: TextRequest):
+    text = request_data.text
+    # Vectorize the example text
+    text_vectorized = vectorizer.transform([text])
+    # Find the nearest neighbors (similar job descriptions)
+    similar_jobs_indices = knn.kneighbors(text_vectorized, n_neighbors=13, return_distance=False)
+    # Get the job titles and descriptions of the similar jobs
+    similar_jobs = jobs_df.iloc[similar_jobs_indices[0]]
+
+    
+    return {"jobs": similar_jobs.drop_duplicates(subset="Job Title") }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Start the FastAPI application
+    uvicorn.run(app, host="0.0.0.0", port=8090)
